594e831a55bd92899afe7984d3e7d38aad8427e2
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-09-01 19:18:35 +0000

    let, const, var

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const id = 1234;
+let userName = "ritank";
+let accountState;  //undefined
+var email = "ritank@gmail.com"; //outdated (never use)
+                                //because of use in block scope 
+                                //and functional scope
+
+random = 999;   //new (without using keyword)
+                //its possible but not recommended
+
+//; in JS is not necessary
+
+console.table([id, userName, email, random, accountState]);   //new
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScript
 Learning JavaScript Again...
+
+From Hitesh Chaudry- Chai aur code.
+
+Why learning JS?
+If you want to learn just coding, then don't. If you want to built great project/product then only proceed.
